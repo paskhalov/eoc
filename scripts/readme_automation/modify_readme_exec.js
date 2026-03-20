b4f6930c0bde85086d7515488b3ae56f050dc038
Vladimir Paskhalov <vpaskhalov@gmail.com>
2026-03-20 08:37:23 +0200

    refactor: common main call

--- scripts/readme_automation/modify_readme_exec.js
+++ scripts/readme_automation/modify_readme_exec.js
@@ -10,19 +10,23 @@ const { parseBlock } = require("./help_to_markdown");
 const { captureCommandOutput } = require("./pty_capture");
 const { updateSection } = require("./inject_readme_section");
 
-(async function (){
-    
+async function main (){
+
     let tmp_data = {};
     let help_text = await captureCommandOutput("eoc", ["--help"]);
     help_text = help_text.replace(/\r\n/g, "\n");
     assert.ok(["Options:","Commands:"].every(sub => help_text.includes(sub)),'"eoc --help" should includes Commands and Options');
-    
+
     tmp_data['HELP_TEXT'] = help_text;
     const res = parseBlock(['Commands:','Command'],help_text);
     //console.log(res);
     //fs.writeFileSync('/tmp/COMMANDS_MARKDOWN', res);
-    
+
     let readMeContent = fs.readFileSync('fake_readme.md', "utf8");
     readMeContent = updateSection('commands', res, readMeContent);
     console.log(readMeContent);
-})();
+}
+
+if (require.main === module) {
+  main();
+};
