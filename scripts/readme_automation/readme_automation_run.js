7136556ff9e80a175af67b24b7324aedeaa49318
Vladimir Paskhalov <vpaskhalov@gmail.com>
2026-03-25 10:59:20 +0200

    refactor: more asserts

--- scripts/readme_automation/readme_automation_run.js
+++ scripts/readme_automation/readme_automation_run.js
@@ -10,15 +10,17 @@ const { parseBlock, bulletListTemplate, updateSection } = require("./help_to_mar
 const { getHelp } = require("../../src/eoc");
 
 function main (){
-  const tmp_data = {};
   let help_text = getHelp();
   help_text = help_text.replace(/\r\n/g, "\n");
   assert.ok(["Options:","Commands:"].every(sub => help_text.includes(sub)),'"eoc --help" should includes Commands and Options');
-  tmp_data.HELP_TEXT = help_text;
   const commands = parseBlock('Commands:',help_text);
+  assert.ok(commands.length > 0,'Commands should have rows')
   const commandsMarkdown = bulletListTemplate(commands);
+  assert.ok(commandsMarkdown.length > 0,"commandsMarkdown result should have text")
   let readMeContent = fs.readFileSync('README.md', "utf8");
+  assert.ok(readMeContent.length > 0,"readMeContent should have text")
   readMeContent = updateSection('commands', commandsMarkdown, readMeContent);
+  assert.ok(readMeContent.length > 0,"readMeContent should sill have text after modification")
   console.log(readMeContent);
 }
 
